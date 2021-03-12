f426baae6b428a8b54af30a1bfece53fb3ec0bdf
Andreas Remdt <me@andreasremdt.com>
2021-03-12 16:14:04 +0100

    deps: upgrade fontsource

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
-import "fontsource-merriweather/900.css";
-import "fontsource-poppins";
-import "fontsource-poppins/600.css";
-import "fontsource-source-code-pro";
+import "@fontsource/merriweather/900.css";
+import "@fontsource/poppins";
+import "@fontsource/poppins/600.css";
+import "@fontsource/source-code-pro";
 import "prismjs/themes/prism-solarizedlight.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
