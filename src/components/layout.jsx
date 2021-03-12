839c169be5d1f8ea843610c82911bc29a31b3081
Andreas Remdt <me@andreasremdt.com>
2021-03-12 20:44:58 +0100

    feat: add favicons and manifest

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -16,6 +16,12 @@ const Layout = ({ children, pageTitle = "Home" }) => {
         </title>
         <meta name="description" content={description} />
         <meta name="author" content="Andreas Remdt" />
+        <meta name="theme-color" content="#f8b73f" />
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Helmet>
       <Header />
       <main>{children}</main>
