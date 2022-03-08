3b3de84d18488462569c774b0e7f2a802b807b53
Andreas Remdt <me@andreasremdt.com>
2022-03-08 14:51:20 +0100

    fix: use Merriweather font correctly

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: 'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-      serif: 'Merriwheather, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+      serif: 'Merriweather, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
     },
     extend: {},
   },
