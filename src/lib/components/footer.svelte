1c05577fd9596b5cb46cbc6576ff16e6547d0456
Andreas Remdt <me@andreasremdt.com>
2022-03-09 20:07:30 +0100

    feat: update footer links

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,4 +1,4 @@
-<footer class="max-w-6xl mx-auto px-4 py-6 text-center text-sm sm:text-base">
+<footer class="max-w-6xl mx-auto px-4 py-6 text-center text-xs sm:text-sm">
   <p>&copy; {new Date().getFullYear()} Andreas Remdt. All Rights Reserved. Licensed under MIT.</p>
   <p>
     Powered by <a
@@ -7,11 +7,22 @@
       rel="noopener nofollow noreferrer"
       class="link">SvelteKit</a
     >,
-    <a href="https://netlify.com" target="_blank" rel="noopener nofollow noreferrer" class="link"
-      >Netlify</a
+    <a href="https://vercel.com" target="_blank" rel="noopener nofollow noreferrer" class="link"
+      >Vercel</a
+    >,
+    <a href="https://graphcms.com" target="_blank" rel="noopener nofollow noreferrer" class="link"
+      >GraphCMS</a
     >, and
     <a href="https://dnsimple.com" target="_blank" rel="noopener nofollow noreferrer" class="link"
       >dnsimple</a
     >.
+  </p>
+  <p>
+    <a
+      href="https://github.com/andreasremdt/andreasremdt.com"
+      target="_blank"
+      rel="noopener nofollow noreferrer"
+      class="link">View source code</a
+    >
   </p>
 </footer>
