dda7d9dbc75c788539c79cbe6680b677800caa41
Andreas Remdt <me@andreasremdt.com>
2022-10-16 17:18:27 +0200

    feat: compress assets via `astro-compress`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ config: { applyBaseStyles: false } })],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    compress(),
+  ],
 });
